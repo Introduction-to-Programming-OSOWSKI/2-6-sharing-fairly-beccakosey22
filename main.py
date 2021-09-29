c707418863d806d25ff1425df8c0400355674ad6
beccakosey22 <820512@mystma.org>
2021-09-29 09:38:08 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function called shareFair() that returns true/false if a given variable x can be fairly divided by a given variable y with no remainder
+def shareFair(x,y):
+    if x % y == 0:
+        return True
+    else:
+        return False
